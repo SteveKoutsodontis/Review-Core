47491e6e9d6c9875f458968e5cbc3957eda88bef
namagaii <rnoel2125@gmail.com>
2021-12-17 13:18:10 -0500

    Completed login page

--- public/js/login.js
+++ public/js/login.js
@@ -1,20 +1,22 @@
 const loginFormHandler = async function(event) {
   event.preventDefault();
 
-  const usernameEl = document.querySelector('#username-input-login');
+  const emailEl = document.querySelector('#email-input-login');
   const passwordEl = document.querySelector('#password-input-login');
 
-  const response = await fetch('/api/user/login', {
+  const response = await fetch('/api/users/login', {
     method: 'POST',
     body: JSON.stringify({
-      username: usernameEl.value,
+      email: emailEl.value,
       password: passwordEl.value,
     }),
     headers: { 'Content-Type': 'application/json' },
   });
 
   if (response.ok) {
-    document.location.replace('/dashboard');
+    let userData = await response.json();
+    sessionStorage.setItem("UserSession", JSON.stringify(userData));
+    document.location.replace('../index.html');
   } else {
     alert('Failed to login');
   }
@@ -23,3 +25,38 @@ const loginFormHandler = async function(event) {
 document
   .querySelector('#login-form')
   .addEventListener('submit', loginFormHandler);
+
+function sleep(ms){
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+const signInFormHandler = async function(event){
+  event.preventDefault();
+  
+  const emailEl = document.querySelector('#email-input-signin');
+  const usernameEl = document.querySelector('#username-input-signin');
+  const passwordEl = document.querySelector('#password-input-signin');
+
+  const response = await fetch('/api/users/', {
+    method: 'POST',
+    body: JSON.stringify({
+      email: emailEl.value,
+      username: usernameEl.value,
+      password: passwordEl.value
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    let userData = await response.json();
+    sessionStorage.setItem("UserSession", JSON.stringify(userData));
+    document.location.replace('../index.html');
+  } else {
+    alert('Failed to signin');
+  }
+}
+
+document
+  .querySelector("#signup-form")
+  .addEventListener('submit', signInFormHandler);
