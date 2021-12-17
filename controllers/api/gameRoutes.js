b2c804b22b1a31bc8bf01e65433843e1e02ead8f
namagaii <rnoel2125@gmail.com>
2021-12-17 14:01:22 -0500

    Added get route to games

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -1,3 +1,26 @@
-//TODO Noel: Create api routes for getting, posting, and deleting comment information from the server //No reference yet maybe wait on this one
+const router = require('express').Router();
+const { Game } = require('../../models');
+// get games
+router.get('/', async (_req, res) => {
+    try{
+        let gameData = await Game.findAll();
+        res.status(200).json(gameData);
+    } catch(error){
+        res.status(500).json(error);
+    }
+});
 
-// get games 
+router.get('/:id', async (req, res) => {
+    try{
+        //if req.params.id !== null
+        //data = Game.findOne({where: {id: req.params.id}})
+        //else
+        //data = Game.findOne({where: {name: req.body.name}})
+        //if !data
+        //api request
+        //Game.create(api info)
+        res.status(200).json({message:"Nothing to see here"});
+    } catch(error){
+        res.status(500).json({message:"IDK how this is error"});
+    }
+});
